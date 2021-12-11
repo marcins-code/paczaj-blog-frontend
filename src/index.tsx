9db940bd949653f3b0aab640aa9e0c190b2fd8b5
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2021-12-11 21:22:20 +0100

    add css for icons

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalTemplate from './templates/GlobalTemplate';
-import './index.css';
+import './assets/css/icomoon.css';
 import App from './App';
 
 ReactDOM.render(
