08a625c9588938d7e6aeb37ce8644f6e4151f18c
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2021-12-11 19:47:57 +0100

    Add background images

---
+++ src/themes/backgroundImages.ts
@@ -0,0 +1,9 @@
+import nastyFabric from '../assets/images/background/nasty-fabric.png';
+import debutLight from '../assets/images/background/debut-light.png';
+import firstAidKit from '../assets/images/background/first-aid-kit.png';
+
+export const backgroundImages = {
+  nastyFabric,
+  debutLight,
+  firstAidKit,
+};
